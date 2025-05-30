c1b2dc0699c95cf312e8962bbe37b9017b014e2e
Ingrid von Baranow <ingrid_baranow@hotmail.com>
2025-05-30 15:11:38 -0300

    Initial Commit

---
+++ MovieFlixTests/MovieFlixTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFlixTests.swift
+//  MovieFlixTests
+//
+//  Created by Ingrid Baranow on 30/05/25.
+//
+
+import Testing
+@testable import MovieFlix
+
+struct MovieFlixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
