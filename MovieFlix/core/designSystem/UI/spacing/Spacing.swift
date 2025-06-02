9a0c829c53d0463a31971ff4027c6c113e220c0d
Ingrid von Baranow <ingrid_baranow@hotmail.com>
2025-06-02 15:13:06 -0300

    Create Spacing.swift

---
+++ MovieFlix/core/designSystem/UI/spacing/Spacing.swift
@@ -0,0 +1,26 @@
+//  Spacing.swift
+//  MovieFlix
+//
+//  Created by Ingrid Baranow on 30/05/25.
+
+import UIKit
+import SnapKit
+
+extension Int {
+    static let none: Int = 0
+    static let quark = 4
+    static let xs = 8
+    static let s1 = 12
+    static let s2 = 16
+    static let m1 = 20
+    static let m2 = 24
+    static let m3 = 28
+    static let l1 = 32
+    static let l2 = 36
+    static let xl1 = 40
+    static let xl2 = 44
+    static let xl3 = 48
+    static let xxl1 = 52
+    static let xxl2 = 56
+    static let xxl3 = 60
+}
