a813510db3a7961caeb4f010db565619e019ce86
Ingrid von Baranow <ingrid_baranow@hotmail.com>
2025-06-02 14:16:52 -0300

    Font + FontType + FontSize

---
+++ MovieFlix/core/designSystem/UI/font/FontSize.swift
@@ -0,0 +1,17 @@
+//  FontSize.swift
+//  MovieFlix
+//
+//  Created by Ingrid Baranow on 30/05/25.
+
+import Foundation
+
+extension CGFloat {
+    static let caption2: CGFloat = 8
+    static let caption1: CGFloat = 10
+    static let body3: CGFloat = 12
+    static let body2: CGFloat = 14
+    static let body1: CGFloat = 16
+    static let headline3: CGFloat = 20
+    static let headline2: CGFloat = 24
+    static let headline1: CGFloat = 32
+}
---
+++ MovieFlix/core/designSystem/UI/font/FontType.swift
@@ -0,0 +1,13 @@
+//  FontType.swift
+//  MovieFlix
+//
+//  Created by Ingrid Baranow on 30/05/25.
+
+import Foundation
+
+extension String {
+    static let light = "Montserrat-Light"
+    static let regular = "Montserrat-Regular"
+    static let bold = "Montserrat-Bold"
+    static let extraBold = "Montserrat-ExtraBold"
+}
