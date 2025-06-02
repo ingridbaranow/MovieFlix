16ae8baa86ad93ed166222fe027f8a89675f6b76
Ingrid von Baranow <ingrid_baranow@hotmail.com>
2025-06-02 15:13:42 -0300

    Create MovieFlixColors.swift

---
+++ MovieFlix/core/designSystem/UI/MovieFlixColors.swift
@@ -0,0 +1,17 @@
+//  MovieFlixColors.swift
+//  MovieFlix
+//
+//  Created by Ingrid Baranow on 30/05/25.
+
+import UIKit
+
+extension UIColor {
+    static let primary = UIColor(hex: "#FFA800")
+    static let primaryDark = UIColor(hex: "#FFC700")
+    static let backgroundDark =  UIColor(hex: "#0D0D0D")
+    static let backgroundLightDark =  UIColor(hex: "#1C1C1E")
+    static let primaryText = UIColor(hex: "#FFFFFF")
+    static let secondaryText = UIColor(hex: "#191919")
+    static let thirdText = UIColor(hex: "#F1C40F")
+    static let textPlaceHolders = UIColor(hex: "#A8A8A8")
+}
