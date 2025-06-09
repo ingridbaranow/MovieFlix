e2ddce01277ea62f2e1bd588005d87f2c1ce8c9a
Ingrid von Baranow <ingrid_baranow@hotmail.com>
2025-06-09 13:30:06 -0300

    GradientImage 1 e 2

---
+++ MovieFlix/core/designSystem/UI/components/GradientImageView.swift
@@ -0,0 +1,37 @@
+//  GradientImageView.swift
+//  MovieFlix
+//
+//  Created by Ingrid Baranow on 30/05/25.
+
+import UIKit
+
+class GradientImageView: UIImageView {
+    
+    private let gradientLayer = CAGradientLayer()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupGradient()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupGradient()
+    }
+    
+    private func setupGradient() {
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.black.withAlphaComponent(0.5).cgColor
+        ]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.1)
+        layer.addSublayer(gradientLayer)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = bounds
+        gradientLayer.frame = CGRect(x: 0, y: bounds.height * 0.4, width: bounds.width, height: bounds.height)
+    }
+}
